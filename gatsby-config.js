bb7455c3cda71eacfd1770d2b84c0cf5502bfc2e
Cengiz Dönmez <c.donmezzz@gmail.com>
2022-05-21 21:34:33 +0300

    graphql data & map gatsby-img

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `gallery`,
+        path: `${__dirname}/src/gallery`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,39 @@
 import * as React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import './index.css'
 
-const IndexPage = () => {
-  return(
-    <div>
-      <h1>TEST</h1>
+const IndexPage = ({data}) => {
+  console.log(data)
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>Merhaba, Ben Cengiz</h1>
+        <p>Bu bir test uygulamasıdır. Gatsby ile kendi çekimlerimi yayınlamayı planlıyorum.</p>
+        <div className="photos">
+          {data.allFile.edges.map(({ node }) =>{
+            return  <Img key={node.id} fluid={node.childImageSharp.fluid} />
+          })}
+        </div>
+      </div>
     </div>
   )
 }
 
+export const pageQuery = graphql`
+  {
+    allFile(filter: {absolutePath: {regex: "//gallery//"}}) {
+      edges {
+        node {
+          id
+          childImageSharp{
+            fluid (maxWidth: 3024) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 export default IndexPage
